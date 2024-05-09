6381ea06a1e2cd5ff15e522cd3172e2b66a2912b
Lawalash <104632797+Lawalash@users.noreply.github.com>
2024-05-09 16:36:31 -0300

    atualizado

--- api/index.php
+++ api/index.php
@@ -1,15 +1,2 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Teste PHP no Vercel</title>
-</head>
-<body>
-    <h1>Olá, mundo!</h1>
-    <p>Este é um teste simples de PHP no Vercel.</p>
-    <?php
-    echo "<p>Agora estou executando PHP no Vercel!</p>";
-    ?>
-</body>
-</html>
+<?php
+echo "Olá, mundo! Este é um arquivo PHP rodando na Vercel.";
